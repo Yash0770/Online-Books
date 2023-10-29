50fc537fc927462a9337ed9f41e63dcf263b5a7b
Yash <yashsolanki0728@gmail.com>
2023-10-29 22:35:29 +0530

    changes in onlineBook component

--- src/_Components/modules/Books/OnlineBooks.jsx
+++ src/_Components/modules/Books/OnlineBooks.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import read from "../../../imgs/read.png";
 import ai from "../../../imgs/ai.jpeg";
 
@@ -7,50 +8,52 @@ function OnlineBooks() {
     <div className="container-fluid mt-4">
       <div className="container">
         <div className="row px-5 py-2">
-          <div
-            className="card mx-2 my-2"
+        <div
+            className="card p-1 col-lg-3 mx-2 my-2"
             style={{
-              width: "18rem",
+              width: "14rem",
+              height: '20rem'
             }}
           >
             <img
               src={read}
-              style={{ width: "100%", height: "200px", objectFit: "cover" }}
+              style={{ width: "94%", height: "150px", marginTop: '6px', marginLeft:'6px', 
+            }}
               className="card-img-top"
               alt="book-img"
             />
-            <div className="card-body">
+            <div className="card-body p-2">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Some quick example text to build on the
               </p>
-              <a href="_" className="btn btn-primary">
-                Go somewhere
-              </a>
+              <div style={{textAlign:'center'}}>
+              <Link to="/online_books" className="btn btn-primary">Go somewhere</Link>
+              </div>
             </div>
           </div>
           <div
-            className="card mx-2 my-2"
+            className="card p-1 col-lg-3 mx-2 my-2"
             style={{
-              width: "18rem",
+              width: "14rem",
+              height: '20rem'
             }}
           >
             <img
               src={ai}
-              style={{ width: "100%", height: "200px", objectFit: "cover" }}
+              style={{ width: "94%", height: "150px", marginTop: '6px', marginLeft:'6px', 
+            }}
               className="card-img-top"
               alt="book-img"
             />
-            <div className="card-body">
+            <div className="card-body p-2">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Some quick example text to build on the
               </p>
-              <a href="_" className="btn btn-primary">
-                Go somewhere
-              </a>
+              <div style={{textAlign:'center'}}>
+              <Link to="/online_books" className="btn btn-primary">Go somewhere</Link>
+              </div>
             </div>
           </div>
         </div>
